e573a3b337d9d19092f370ae0dd8f21ee0ed6241
szxh <1439051185@qq.com>
2016-08-17 23:59:02 +0800

    Add files via upload

--- task3-38/task3_38.js
+++ task3-38/task3_38.js
@@ -7,9 +7,26 @@ var table = {
 	
 	//配置表格的单元格数据，为二重数组
 	td: [
-			['小明', '80', '90', '70', '240'],
-			['小红', '90', '60', '90', '240'],
-			['小亮', '60', '100', '70', '230']
+			['小明', '820', '97', '744', '2487'],
+			['小红', '904', '625', '978', '2414'],
+			['小亮', '632', '114', '72024', '234'],
+			['小蛤', '110', '150', '301', '101'],
+			['小哈', '11', '1450', '31', '1'],
+			['小明', '820', '97', '744', '2487'],
+			['小红', '904', '625', '978', '2414'],
+			['小亮', '632', '114', '72024', '234'],
+			['小蛤', '110', '150', '301', '101'],
+			['小哈', '11', '1450', '31', '1'],
+			['小明', '820', '97', '744', '2487'],
+			['小红', '904', '625', '978', '2414'],
+			['小亮', '632', '114', '72024', '234'],
+			['小蛤', '110', '150', '301', '101'],
+			['小哈', '11', '1450', '31', '1'],
+			['小明', '820', '97', '744', '2487'],
+			['小红', '904', '625', '978', '2414'],
+			['小亮', '632', '114', '72024', '234'],
+			['小蛤', '110', '150', '301', '101'],
+			['小哈', '11', '1450', '31', '1']
 		],
 	
 	//需要排序序列的标题名
@@ -25,61 +42,66 @@ function $(selector) {
 }
 
 var tableSort = $('#table-sort');
-
 EventUtil.addHandler(tableSort, 'click', sortUD);
+EventUtil.addHandler(document, 'scroll', fix);
 
+//排序
 function sortUD(event) {
 	event = EventUtil.getEvent(event);
 	var target = EventUtil.getTarget(event);
 	if (target.classList.contains('triangle-up')) {
-		var headValue = target.parentNode.firstChild.nodeValue;
+		var headValue = target.parentNode.parentNode.firstChild.nodeValue;
 		if (table.th.indexOf(headValue) !== -1) {
 			var index = table.th.indexOf(headValue);
 		}
-		for (var i = table.td.length-1; i > 0; i--) {
-			for (var j = 0; j < i; j++) {
-				if (table.td[j][index] < table.td[j+1][index]) {
-					swap(table.td[j],table.td[j+1]);
-				}
-			}
-		}
+		table.td.sort(function(a, b) {
+			return a[index] - b[index];
+		});
+		render();
 	}
 	if (target.classList.contains('triangle-down')) {
-		var headValue = target.parentNode.firstChild.nodeValue;
+		var headValue = target.parentNode.parentNode.firstChild.nodeValue;
 		if (table.th.indexOf(headValue) !== -1) {
 			var index = table.th.indexOf(headValue);
 		}
-		for (var i = table.td.length-1; i > 0; i--) {
-			for (var j = 0; j < i; j++) {
-				if (table.td[j][index] > table.td[j+1][index]) {
-					swap(table.td[j],table.td[j+1]);
-				}
-			}
-		}
+		table.td.sort(function(a, b) {
+			return b[index] - a[index];
+		});
+		render();
 	}
-	render();
-}
-
-function swap(a, b) {
-	var temp;
-	temp = a;
-	a = b;
-	b = temp;
 }
 
 //确定需要排序的列，给其标题加上排序符号
 function render() {
-	var headHTML = '<tr>' + table.th.map(function(item,index) {
-		return '<th>' + item + '</th>';
-	}).join('') + '</tr>';
-	var dataHTML;
-	for (var i = 0; i < table.td.length; i++) {
-		for (var j = 0; j < table.td[i].length; j++) {
-			var text = '<td>' + table.td[i][j] + '</td>';
+	var headHTML = '<tr class="title">' + table.th.map(function(item) {
+		if (table.sortCol.indexOf(item) === -1) {
+			return '<th>' + item + '</th>';
+		} else {
+			return '<th>' + item + '<span id="triangle-wrap"><span class="triangle-up"></span><span class="triangle-down"></span></span></th>';
 		}
-		dataHTML = '<tr>' + text + '</tr>';
+	}).join('') + '</tr>';
+	var dataHTML = '';
+	for (var i = 0; i < table.td.length; i++) {
+		var text = '';
+		for (var j = 0; j < table.td[i].length; j++) {
+			text += '<td>' + table.td[i][j] + '</td>';
+		}
+		dataHTML += '<tr>' + text + '</tr>';
 	}
 	tableSort.innerHTML = headHTML + dataHTML;
 }
 
 render();
+
+function fix() {
+	console.log(document.body.scrollTop, tableSort.offsetTop);
+	if (tableSort.offsetTop < document.body.scrollTop && !$('.title').classList.contains('fixed')) {
+		$('.title').classList.add('fixed');
+	}
+	if (tableSort.offsetTop + tableSort.offsetHeight < document.body.scrollTop) {
+		$('.title').classList.remove('fixed');
+	}
+	if (tableSort.offsetTop > document.body.scrollTop) {
+		$('.title').classList.remove('fixed');
+	}
+}
