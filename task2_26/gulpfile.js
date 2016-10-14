c567a4792c44735276bd65ee9d7fd73c93b24583
szxh <1439051185@qq.com>
2016-10-14 15:31:05 +0800

    Add files via upload

--- task2_26/gulpfile.js
+++ task2_26/gulpfile.js
@@ -23,7 +23,6 @@ gulp.task('less', function() {
 gulp.task('scripts', function() {
     return gulp.src('./src/js/*.js')
         .pipe(concat('all.min.js'))
-        .pipe(uglify())
         .pipe(gulp.dest('./dist/js'));
 });
 
